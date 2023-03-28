3ff7c7c45cf9d28fbe8839ffac3999e15006fc7b
Klok <klok1305@gmail.com>
2023-03-28 17:06:38 +0300

    Lab 2 fixed

--- Лаба 2/хорды.py
+++ Лаба 2/хорды.py
@@ -13,26 +13,21 @@ def df(x):
     return math.exp(x) - 1 / x ** 2
 
 
-def tangent_method(a, b, eps):
+def secant_method(a, b, eps):
     global k
-    Xmin = 0
-    c = (f(b) - f(a) + df(a) * a - df(b) * b) / (df(a) - df(b))
-    while abs(df(c)) > eps and (b - a) > eps:
+    a1, a2, = a, b
+    a3 = a2 - df(a2) * (a1 - a2) / (df(a1) - df(a2))
+    while abs(df(a3)) > eps or abs(a1 - a2) > eps:
         k += 1
-        if df(c) == 0:
-            Xmin = c
-            break
-        elif df(c) > 0:
-            b = c
-        else:
-            a = c
-        c = (f(b) - f(a) + df(a) * a - df(b) * b) / (df(a) - df(b))
-        Xmin = c
-    return Xmin
+        a1 = a2
+        a2 = a3
+        a3 = a2 - df(a2) * (a1 - a2) / (df(a1) - df(a2))
+
+    return a3
 
 
 q, k = 0, 0
 a, b = 0.5, 1.0
-x_min = tangent_method(a, b, 0.0001)
+x_min = secant_method(a, b, 0.0001)
 print("Минимум функции f(x) на отрезке [{}, {}]: x = {}, f(x) = {}".format(a, b, x_min, f(x_min)))
 print("Кол во итераций {}, кол-во исчислений функций {}".format(k, q))
